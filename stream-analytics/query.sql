4958162f0d04dc8f01eb112fe6225eb482e0a175
saraMir26 <mirz0014@algonquinlive.com>
2026-04-13 01:09:08 -0400

    a

--- stream-analytics/query.sql
+++ stream-analytics/query.sql
@@ -1,1 +1,61 @@
--- Stream Analytics query will go here
+WITH AggregatedReadings AS
+(
+    SELECT
+        location,
+        System.Timestamp() AS windowEndTime,
+        AVG(iceThickness) AS avgIceThickness,
+        MIN(iceThickness) AS minIceThickness,
+        MAX(iceThickness) AS maxIceThickness,
+        AVG(surfaceTemperature) AS avgSurfaceTemperature,
+        MIN(surfaceTemperature) AS minSurfaceTemperature,
+        MAX(surfaceTemperature) AS maxSurfaceTemperature,
+        MAX(snowAccumulation) AS maxSnowAccumulation,
+        AVG(externalTemperature) AS avgExternalTemperature,
+        COUNT(*) AS readingCount
+    FROM SensorInput TIMESTAMP BY timestamp
+    GROUP BY
+        location,
+        TumblingWindow(minute, 5)
+)
+
+SELECT
+    CONCAT(REPLACE(location, ' ', ''), '-', CAST(windowEndTime AS nvarchar(max))) AS id,
+    location,
+    windowEndTime,
+    avgIceThickness,
+    minIceThickness,
+    maxIceThickness,
+    avgSurfaceTemperature,
+    minSurfaceTemperature,
+    maxSurfaceTemperature,
+    maxSnowAccumulation,
+    avgExternalTemperature,
+    readingCount,
+    CASE
+        WHEN avgIceThickness >= 30 AND avgSurfaceTemperature <= -2 THEN 'Safe'
+        WHEN avgIceThickness >= 25 AND avgSurfaceTemperature <= 0 THEN 'Caution'
+        ELSE 'Unsafe'
+    END AS safetyStatus
+INTO CosmosOutput
+FROM AggregatedReadings;
+
+SELECT
+    CONCAT(REPLACE(location, ' ', ''), '-', CAST(windowEndTime AS nvarchar(max))) AS id,
+    location,
+    windowEndTime,
+    avgIceThickness,
+    minIceThickness,
+    maxIceThickness,
+    avgSurfaceTemperature,
+    minSurfaceTemperature,
+    maxSurfaceTemperature,
+    maxSnowAccumulation,
+    avgExternalTemperature,
+    readingCount,
+    CASE
+        WHEN avgIceThickness >= 30 AND avgSurfaceTemperature <= -2 THEN 'Safe'
+        WHEN avgIceThickness >= 25 AND avgSurfaceTemperature <= 0 THEN 'Caution'
+        ELSE 'Unsafe'
+    END AS safetyStatus
+INTO BlobOutput
+FROM AggregatedReadings;
